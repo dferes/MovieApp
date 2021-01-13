b01671b6fdd2e6772305b7474b54f962ba27815f
dferes <dferes23@gmail.com>
2021-01-12 16:23:54 -0800

    modified what was optional

--- forms.py
+++ forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired, Email, URL, Optional
+from wtforms.validators import DataRequired, Email, URL, Optional, Length
 
 
 class NewUserForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('E-mail', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[Length(min=6)])
+    password = PasswordField('Password', validators=[Length(min=7)])
     user_pic_url = StringField('(Optional) Image URL', validators=[URL(), Optional()])
