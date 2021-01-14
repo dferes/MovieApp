30bb2442fc54650a868efbeee929097753c3da0e
dferes <dferes23@gmail.com>
2021-01-14 00:33:47 -0800

    added a from for editing a user

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, TextAreaField
 from wtforms.validators import DataRequired, Email, URL, Optional, Length
 
 
@@ -15,3 +15,13 @@ class UserLoginForm(FlaskForm):
     
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[Length(min=7)])
+
+
+class EditUserForm(FlaskForm):
+    """ Form for editing and adding user bio, location, and a header image URL """
+    username = StringField('Username')
+    email = StringField('E-mail', validators=[Optional(), Email()])
+    user_pic_url = StringField('Image URL', validators=[URL(), Optional()])
+    bio = TextAreaField('Bio', validators=[Optional()])
+    header_image_url = StringField('Header Image', validators=[URL(), Optional()])
+    password = PasswordField('Password', validators=[Length(min=7), DataRequired()])
