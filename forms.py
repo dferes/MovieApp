5c7cd713017817b12ff7cf1762e59cf512560f26
dferes <dferes23@gmail.com>
2021-01-13 16:11:33 -0800

    added a user login form

--- forms.py
+++ forms.py
@@ -4,7 +4,14 @@ from wtforms.validators import DataRequired, Email, URL, Optional, Length
 
 
 class NewUserForm(FlaskForm):
+    
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('E-mail', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[Length(min=7)])
     user_pic_url = StringField('(Optional) Image URL', validators=[URL(), Optional()])
+    
+    
+class UserLoginForm(FlaskForm):
+    
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[Length(min=7)])
