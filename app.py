6ed42e3f084d712bfa254a0873c050da07ebba4f
dferes <dferes23@gmail.com>
2021-01-13 20:05:11 -0800

    added endpoints for user profile, user lists, user following, user followers

--- app.py
+++ app.py
@@ -10,12 +10,14 @@ from sqlalchemy.exc import IntegrityError
 from forms import NewUserForm, UserLoginForm
 from user_functions import signup, authenticate
 
+#k_jg1h63to
+
 
 CURRENT_USER_KEY = 'current_user'
 # Note that you are limited to 100 API calls per day...
 base_url = f"https://imdb-api.com/en/API/Search/{api_key}"
 cast_url = f"https://imdb-api.com/en/API/FullCast/{api_key}"
-wikipedia_url = f"https://imdb-api.com/en/API/Wikipedia/{api_key}" # followed by imdb id
+wikipedia_url = f"https://imdb-api.com/en/API/Wikipedia/{api_key}"
 
 app = Flask(__name__)
 
@@ -40,7 +42,11 @@ def do_logout():
         
 @app.route('/')
 def homepage():
-    user = session.get(CURRENT_USER_KEY)
+    if CURRENT_USER_KEY in session:
+        user = User.query.get_or_404(session[CURRENT_USER_KEY])
+    else:
+        user = None
+        
     return render_template('homepage.html', user=user)
 
 
@@ -80,8 +86,9 @@ def login():
         user = authenticate( form.username.data, form.password.data)
         if user:
             do_login(user)
-            # flash message here
-            return redirect('/')
+            flash(f"Welcome back {user.username}", 'success')
+            return redirect(f"/users/{user.id}")
+        
         flash('Invalid credentials. Please try again.', 'danger')
     
     return render_template('/signup_and_login/login.html', form=form) 
@@ -91,21 +98,23 @@ def login():
 def logout():
     user = User.query.get_or_404(session[CURRENT_USER_KEY])
     do_logout()
+    
     return redirect('/')
 
 
 @app.route('/get-movie-by-query', methods=['GET'])
 def get_move_by_query():
+    user = User.query.get_or_404(session[CURRENT_USER_KEY])
     title = request.args.get('q') # just assume the query is a title for now
     res = requests.get(f"http://127.0.0.1:5000/api/get-movie-by-title/{title}") # Only works locally; fix this to accomodate bothe cases 
     res = json.loads(res.text)['results']
     # print(f"----------------------------------\n{res}\n--------------------------------------")
-    return render_template('search/show-query-results.html', res=res, query=title)
+    return render_template('search/show-query-results.html', res=res, query=title, user=user)
 
 
 @app.route('/show-movie-details/<string:id>')
 def show_movie_details(id):
-    # print(f"The ID is: {id}")
+    user = User.query.get_or_404(session[CURRENT_USER_KEY])
     res = requests.get(f"http://127.0.0.1:5000/api/get-cast-information/{id}") # Only works locally
     res = json.loads(res.text)
     # print('---------------------------------',res, '------------------------------')
@@ -130,12 +139,32 @@ def show_movie_details(id):
         directors=directors, 
         writers=writers,
         actors=actors,
-        plot=plot)
+        plot=plot,
+        user=user)
+
 
 @app.route('/users/<int:id>')
 def show_users_own_profile(id):
     user = User.query.get_or_404(session.get(CURRENT_USER_KEY))
-    return render_template('user/user_profile.html', user=user)
+    return render_template('user/show_profile_details.html', user=user)
+
+
+@app.route('/users/<int:id>/my-lists', methods=['GET'])
+def show_user_lists(id):
+    user = User.query.get_or_404(session[CURRENT_USER_KEY])
+    return render_template('user/show_user_lists.html', user=user)
+
+
+@app.route('/users/<int:id>/following', methods=['GET']) # todo
+def show_user_following(id):
+    user = User.query.get_or_404(session[CURRENT_USER_KEY])
+    return render_template('user/following.html', user=user)
+
+
+@app.route('/users/<int:id>/followers', methods=['GET']) # todo
+def show_user_followers(id):
+    user = User.query.get_or_404(session[CURRENT_USER_KEY])
+    return render_template('user/followers.html', user=user)
 
 #-------------------------------------------------------------------------
 #                         External API calls
