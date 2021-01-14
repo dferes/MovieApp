cc2fab03d2cf81a1022c7614caa22c7bcab0a0ad
dferes <dferes23@gmail.com>
2021-01-14 10:35:25 -0800

    made a new form for making a new movie list

--- forms.py
+++ forms.py
@@ -18,10 +18,16 @@ class UserLoginForm(FlaskForm):
 
 
 class EditUserForm(FlaskForm):
-    """ Form for editing and adding user bio, location, and a header image URL """
+
     username = StringField('Username')
     email = StringField('E-mail', validators=[Optional(), Email()])
     user_pic_url = StringField('Image URL', validators=[URL(), Optional()])
     bio = TextAreaField('Bio', validators=[Optional()])
     header_image_url = StringField('Header Image', validators=[URL(), Optional()])
     password = PasswordField('Password', validators=[Length(min=7), DataRequired()])
+    
+
+class NewListForm(FlaskForm):
+    
+    title = StringField('List Name', validators=[DataRequired()])
+    description = TextAreaField('Description', validators=[Optional()])
