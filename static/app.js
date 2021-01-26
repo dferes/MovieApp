89691e8c7fdfd738ee347e5154f80d1a92088c55
nori <__NORI__@localhost.localdomain>
2021-01-26 11:39:55 -0800

    new comment api call is now made from here instead of through a form directly to the flask app

--- static/app.js
+++ static/app.js
@@ -13,21 +13,43 @@ $('document').ready(function() {
         $('.comment-box-button-container').hide();
     })
 
+    function commentListElementHTML(username, userID, userPicURL, comment) {
+        return `
+        <li class="list-group-item">
+            <a href="/users/${userID}">
+                <img src="${userPicURL }" alt="" class="timeline-image">
+            </a>
+            <div class="message-area">
+                <a href="/users/${userID}">@${username}</a>
+                <span class="text-muted">Now</span>
+                <p>${comment}</p>
+            </div>
+        </li>
+     `;
+    }
 
-    // $(".comment-submit-button").on("click", function(e) {
-    //     // e.preventDefault();
-     
-    //     let comment = $(".comment-box").val();
-    //     console.log(comment);
-    //     const $newP = $('<p>');
-    //     $newP.text($(".comment-box").val());
 
+    $(".comment-submit-button").on("click", async function(e) {
+        e.preventDefault();
         
-    //     // $(".comment-list").prepend(newComment);
-    //     const $newCommentListItem = $('<li>').append($newP)
-    //     $(".comment-list").prepend($newCommentListItem);
-    //     $(".comment-box").val('');
-    //     $('.comment-box-button-container').hide();
-    //   });
+        let username = $(".username").val();
+        let userPicURL = $(".user-pic-url").val();
+        let userID = $(".user-id").val();
+        let content = $(".comment-box").val();
+        let listID = $(".movie_list_id").val();
+        
+        const newCommentResponse = await axios.post('http://127.0.0.1:5000/api/new-comment', {
+            userID, listID, content
+        });
+        
+        if (newCommentResponse.data === true) {
+            let newComment = commentListElementHTML(username, userID, userPicURL, content);
+            $(".comment-list-body").prepend(newComment);
+    
+            $(".comment-box").val('');
+            $('.comment-box-button-container').hide();
+        }
+    
+    });
 
 })
