1ddfd04989f168c96ac602092df2dfd024220ba2
dferes <dferes23@gmail.com>
2021-02-11 16:57:10 -0800

    testing os address variable

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
+import os
+
 api_key = 'k_jg1h63to'
+
+address_ = os.environ.get('REMOTE_ADDR')
+print('------------------------------', address_)
 
 # Note that you are limited to 100 API calls per day...
 base_url = f"https://imdb-api.com/en/API/Search/{api_key}"
@@ -7,7 +12,7 @@ wikipedia_url = f"https://imdb-api.com/en/API/Wikipedia/{api_key}"
 ratings_url = f"https://imdb-api.com/en/API/Ratings/{api_key}"
 actors_url = f"https://imdb-api.com/en/API/Name/{api_key}"
 movie_url = f"https://imdb-api.com/en/API/Title/{api_key}"
-local_url = 'http://127.0.0.1:5000/api/get-movie-details'
+local_url = 'http://{address_}:5000/api/get-movie-details'
 
 URL_DICTIONARY = {
     'base': base_url, 
